cdc09c02711bd1e3abfac3d9f6829bd8b75e6126
JEONWOOJIN <90187250+bingbong916@users.noreply.github.com>
2024-08-25 22:48:54 +0900

    fix: UserRepository JPA 변경

--- src/main/java/novice/present/repository/UserRepository.java
+++ src/main/java/novice/present/repository/UserRepository.java
@@ -1,20 +1,9 @@
 package novice.present.repository;
 
 import novice.present.domain.User;
-import org.springframework.stereotype.Repository;
-
-import java.util.HashMap;
-import java.util.Map;
+import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
-import java.util.concurrent.ConcurrentMap;
 
-@Repository
-public interface UserRepository {
-    public User save(User user);
-
-    public User findById(Long id);
-
-    public void clearStore();
-
-    Optional<User> findByUserLoginIdAndUserPassword(String userLoginId, String userPassword);
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUserLoginIdAndUserPassword(String loginId, String password);
 }
