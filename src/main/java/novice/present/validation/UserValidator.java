a9cde65bbc86dff5b8ff347cb683f3f1fa483ceb
강병현 <kim1228com@naver.com>
2024-08-27 20:43:13 +0900

    회원가입 검증(생일, 성별 제외)

--- src/main/java/novice/present/validation/UserValidator.java
+++ src/main/java/novice/present/validation/UserValidator.java
@@ -7,6 +7,7 @@ import org.springframework.validation.Errors;
 import org.springframework.validation.ValidationUtils;
 import org.springframework.validation.Validator;
 
+import java.time.LocalDate;
 import java.util.regex.Pattern;
 
 @Slf4j
@@ -27,24 +28,44 @@ public class UserValidator implements Validator {
         User user = (User) target;
 
         // userLoginId 검증 - 최대 10자, 중복 안됨
-        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "userLoginId", "required");
+        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "userLoginId", "signup.userLoginId");
         if(user.getUserLoginId().length() > 10){
             log.info("1");
-            errors.rejectValue("userLoginId", "length", "ID는 1~10자리로 이뤄져야 합니다.");
+            errors.rejectValue("userLoginId", "signup.userLoginId.length");
         }
 
-
+        // 패스워드
         if(user.getUserPassword().isEmpty()){
-            errors.rejectValue("userPassword", "pattern", "필수 값 입니다.");
+            errors.rejectValue("userPassword", "signup.password"); //errorCode: pattern
         } //공백 방지
         else if (!Pattern.matches(PASSWORD_PATTERN, user.getUserPassword())) {
             log.info("2");
-            errors.rejectValue("userPassword", "pattern", "비밀번호는 최소 8자리이며 숫자,영문자,특수문자가 포함되어야 합니다.");
+            errors.rejectValue("userPassword", "signup.password.condition");
         } // userPassword 검증 - 숫자, 영어, 특수문자 필수 & 8자 이상 & Hash로 저장 및 비교
         else if (!user.getUserPassword().equals(user.getUserRepeatPassword())) {
             log.info("3");
-            errors.rejectValue("userPassword", "match", "패스워드가 일치하지 않습니다.");
+            errors.rejectValue("userPassword", "signup.password.match", "패스워드가 일치하지 않습니다.");
         } // userPassword와 userRepeatPassword 일치하는지 확인
+
+        // 이메일
+        if(user.getUserEmail().isEmpty()){
+            errors.rejectValue("userEmail", "signup.email");
+        }
+
+        // 닉네임
+        if(user.getUserNickname().isEmpty()){
+            errors.rejectValue("userNickname", "signup.nickname");
+        }
+
+        // 성별
+        if(user.getUserGender() == null){
+            errors.rejectValue("userGender", "signup.gender");
+        }
+
+        // 직업
+        if(user.getUserJob().isEmpty()){
+            errors.rejectValue("userJob", "signup.job");
+        }
 
     }
 }
