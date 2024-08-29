d6cf4206e428b802b9f4a3ba5d372682ad55e821
hahakang7 <hahakang77@gmail.com>
2024-08-29 14:49:25 +0900

    유저 프로필 뷰 프로토타입 제작

--- src/main/java/novice/present/controller/ProfileController.java
+++ src/main/java/novice/present/controller/ProfileController.java
@@ -5,26 +5,17 @@ import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import novice.present.domain.User;
-import novice.present.repository.UserRepository;
-import novice.present.validation.UserValidator;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.InitBinder;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @Slf4j
 @RequiredArgsConstructor
-public class MainController {
+public class ProfileController {
 
-    @GetMapping
-    public String home(Model model, HttpServletRequest request) {
+    @GetMapping("/profile")
+    public String profileTest(Model model, HttpServletRequest request) {
 
         HttpSession session = request.getSession(false);
         User user = null;
@@ -33,10 +24,14 @@ public class MainController {
             user = (User) session.getAttribute(AuthController.LOGIN_MEMBER);
             log.info("메인 컨트롤러 유저: {}", user);
         }
+
+        //유저가 로그인하지 않았으면 url로 접근해도 로그인화면으로
+        if (session == null) {
+            return "redirect:/login";
+        }
+
         model.addAttribute("user", user);
-        return "index";
+
+        return "userProfile";
     }
-
-
-
 }
